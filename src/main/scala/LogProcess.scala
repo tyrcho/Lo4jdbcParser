35819355fbbe537adb4c46770d876859b829e633
MICHEL DAVIOT <michel.daviot@atos.net>
2015-01-20 16:25:08 +0100

    first version

---
+++ src/main/scala/LogProcess.scala
@@ -0,0 +1,68 @@
+import java.io.FileWriter
+
+object LogProcess extends App {
+  val lines = joinLines(io.Source.fromFile("""t:\log4jdbc.txt""").getLines)
+  val out = new FileWriter("t:/analysis.txt")
+
+  def println(a: Any) = {
+    System.out.println(a)
+    out.write(a.toString)
+    out.write("\n")
+    out.flush()
+  }
+
+  val requests = for {
+    l <- lines //.take(10000)
+    e <- lineToEntry(l)
+  } yield e
+
+  for ((key, req) <- requests.toList.groupBy(_.key)) {
+    val count = req.map(_.count).sum
+    println(s"$count $key requests")
+    val total = req.map(_.time).sum / 1000
+    println(s"total time : $total seconds")
+    req.sortBy(-_.time).take(10).foreach(println)
+
+    println("--SAME--")
+    val distinct = for ((req, entries) <- req.groupBy(_.request)) yield Entry(key, entries.map(_.time).sum, req, entries.map(_.count).sum)
+    distinct.toList.sortBy(-_.time).take(10).foreach(println)
+
+    println("--SIMILAR--")
+    val unique = for ((req, entries) <- req.groupBy(_.uniquePart)) yield Entry(key, entries.map(_.time).sum, req, entries.map(_.count).sum)
+    unique.toList.sortBy(-_.time).take(10).foreach(println)
+
+    println()
+  }
+
+  def lineToEntry(l: String) = {
+    val simpleRe = """.* - .*\. (\w+) (.*) \{executed in (\d+) ms\}""".r
+    val batchRe = """.* - .*\. batching (\d*) statements: \d*: (\w+) (.*) \{executed in (\d+) ms\}""".r
+    l match {
+      case batchRe(count, key, request, time) => Some(Entry(key, time.toInt, request, count.toInt))
+      case simpleRe(key, request, time) => Some(Entry(key, time.toInt, request))
+    }
+  }
+
+  def joinLines(lines: Iterator[String]) = new Iterator[String] {
+    def hasNext = lines.hasNext
+    def next = {
+      lines.takeWhile(_ != "").mkString
+    }
+  }
+
+  case class Entry(key: String, time: Int, request: String, count: Int = 1) {
+    override def toString =
+      s"$count $key ($time ms) : $request"
+
+    lazy val uniquePart = {
+      val sep = key match {
+        case "insert" => " values "
+        case "select" | "delete" => " where "
+        case "update" => "="
+        case _ => ""
+      }
+      if (request.contains(sep)) request.substring(0, request.indexOf(sep))
+      else request
+    }
+  }
+}
