8cbc529ef3730f785fb468cd7742eea6ef8702ff
[a518291] Michel Daviot <michel.daviot@atos.net>
2015-09-25 11:40:38 +0200

    usage message for arguments

--- src/main/scala/Config.scala
+++ src/main/scala/Config.scala
@@ -8,8 +8,8 @@ object Config {
   val parser = new scopt.OptionParser[Config]("logparser") {
     arg[String]("[input_file]") action { (x, c) => c.copy(input = x) }
     arg[String]("[output_file]") action { (x, c) => c.copy(output = x) }
-    opt[String]('c', "codec") action { (x, c) => c.copy(codec = x) }
-    opt[Int]('t', "top") action { (x, c) => c.copy(top = x) }
+    opt[String]('c', "codec") text("java Codec for input file, UTF-8 (default) and ISO-8859-1 are supported") action { (x, c) => c.copy(codec = x) }
+    opt[Int]('t', "top") text("top SQL statements to keep in output") action { (x, c) => c.copy(top = x) }
   }
 
   def parse(args: Array[String]) =
