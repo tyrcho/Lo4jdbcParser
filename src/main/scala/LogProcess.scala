f26f4881a760fafdd97b6916769eb50e40737754
[a518291] Michel Daviot <michel.daviot@atos.net>
2015-09-25 11:17:24 +0200

    argument parsing + can specify how many top requests to display

--- src/main/scala/LogProcess.scala
+++ src/main/scala/LogProcess.scala
@@ -1,32 +1,66 @@
 import java.io.FileWriter
 import scala.io.Codec
+import java.io.File
 
 object LogProcess extends App {
-  if (args.size < 2) {
-    System.err.println("Arguments : input_file output_file [Codec]")
-    System.err.println("Codec can be UTF-8 (default) or ISO-8859-1")
-    System.exit(1)
-  }
-  val (input, output) = (args(0), args(1))
-  val codec = if (args.size < 3) Codec.UTF8 else Codec(args(2))
-  val lines = joinLines(io.Source.fromFile(input)(codec).getLines)
-  val out = new FileWriter(output)
-  val topCount = 15
 
-  def println(a: Any) = {
-    System.out.println(a)
-    out.write(a.toString)
-    out.write("\n")
-    out.flush()
+  case class Config(
+    input: String = "",
+    output: String = "",
+    codec: String = "UTF-8",
+    top: Int = 10)
+
+  val parser = new scopt.OptionParser[Config]("logparser") {
+    arg[String]("[input_file]") action { (x, c) => c.copy(input = x) }
+    arg[String]("[output_file]") action { (x, c) => c.copy(output = x) }
+    opt[String]('c', "codec") action { (x, c) => c.copy(codec = x) }
+    opt[Int]('t', "top") action { (x, c) => c.copy(top = x) }
   }
 
-  val requests = (for {
-    l <- lines
-    e <- lineToEntry(l)
-  } yield e).toList
+  parser.parse(args, Config()) match {
+    case Some(config) =>
 
-  val total = requests.map(_.count).sum
-  println(s"$total Total requests")
+      val codec = Codec(config.codec)
+      val lines = joinLines(io.Source.fromFile(config.input)(codec).getLines)
+      val out = new FileWriter(config.output)
+      val topCount = config.top
+
+      def println(a: Any) = {
+        System.out.println(a)
+        out.write(a.toString)
+        out.write("\n")
+        out.flush()
+      }
+
+      val requests = (for {
+        l <- lines
+        e <- lineToEntry(l)
+      } yield e).toList
+
+      val total = requests.map(_.count).sum
+      println(s"$total Total requests")
+
+      for ((key, req) <- requests.toList.groupBy(_.key).toList.sortBy(order)) {
+        val count = req.map(_.count).sum
+        println(s"$count $key requests")
+        val total = req.map(_.time).sum / 1000
+        println(s"total time : $total seconds")
+        println(s"-- TOP $topCount requests, by time --")
+
+        req.sortBy(-_.time).take(topCount).foreach(println)
+
+        println(s"-- TOP $topCount requests, by total time for same requests --")
+        val distinct = for ((r, entries) <- req.groupBy(_.request)) yield Entry(key, entries.map(_.time).sum, r, entries.map(_.count).sum)
+        distinct.toList.sortBy(-_.time).take(topCount).foreach(println)
+
+        println(s"-- TOP $topCount requests, by total time for similar (same statement, different values) requests --")
+        val unique = for ((r, entries) <- req.groupBy(_.uniquePart)) yield Entry(key, entries.map(_.time).sum, entries.head.uniquePart, entries.map(_.count).sum)
+        unique.toList.sortBy(-_.time).take(topCount).foreach(println)
+
+        println()
+      }
+    case None => // arguments are bad, error message will have been displayed
+  }
 
   def order[T](elt: (String, T)): Int = {
     val key = elt._1
@@ -37,26 +71,6 @@ object LogProcess extends App {
       case "delete" => -1
       case _        => key.head.toInt
     }
-  }
-
-  for ((key, req) <- requests.toList.groupBy(_.key).toList.sortBy(order)) {
-    val count = req.map(_.count).sum
-    println(s"$count $key requests")
-    val total = req.map(_.time).sum / 1000
-    println(s"total time : $total seconds")
-    println(s"-- TOP $topCount requests, by time --")
-
-    req.sortBy(-_.time).take(topCount).foreach(println)
-
-    println(s"-- TOP $topCount requests, by total time for same requests --")
-    val distinct = for ((r, entries) <- req.groupBy(_.request)) yield Entry(key, entries.map(_.time).sum, r, entries.map(_.count).sum)
-    distinct.toList.sortBy(-_.time).take(topCount).foreach(println)
-
-    println(s"-- TOP $topCount requests, by total time for similar (same statement, different values) requests --")
-    val unique = for ((r, entries) <- req.groupBy(_.uniquePart)) yield Entry(key, entries.map(_.time).sum, entries.head.uniquePart, entries.map(_.count).sum)
-    unique.toList.sortBy(-_.time).take(topCount).foreach(println)
-
-    println()
   }
 
   def lineToEntry(l: String) = {
